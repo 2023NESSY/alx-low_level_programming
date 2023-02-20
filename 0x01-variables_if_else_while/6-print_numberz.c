ac860577c4f89e060c2721027d6662ee8c9e57bd
Agnes Wangare <agnes.wangare02@gmail.com>
2023-02-20 13:36:56 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
 /**
- *
+ * main - prints single digits below 10
+ * Return: Always 0 (success)
+ */
+int mai(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+	return (0);
+}
