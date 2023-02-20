2c0aefcfd13bb197badb9998be097609e1143efc
Agnes Wangare <agnes.wangare02@gmail.com>
2023-02-20 13:12:10 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints numbers 0 to 9
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+
+	printf("\n");
+
+	return (0);
+}
