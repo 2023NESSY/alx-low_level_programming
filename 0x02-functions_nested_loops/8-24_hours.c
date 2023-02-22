57e23faa931d10df19d497e9d958e5ecbdc5dfe2
Agnes Wangare <agnes.wangare02@gmail.com>
2023-02-21 22:00:44 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,22 @@
 #include "main.h"
 
 /**
- * add - add two numbers
- * @a: the first number
- * @b: the second number
- * Return: the addition of both numbers
+ * jack_bauer - prints every minute of the day
  */
+void jack_bauer(void)
+{
+	int hour, minute;
 
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
